4b1c3c133cfdad67fa86cbc72ada90054c3e110a
Favio Manrrubia <manrrubiaf@gmail.com>
2024-11-25 15:04:25 -0300

    Update main.ts
    port and cors changed

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,13 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser'
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {  
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.urlencoded({ extended: false }));  
   app.use(bodyParser.json());
-  app.enableCors()
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+
+  })
   await app.listen(PORT);
 }
 bootstrap();
